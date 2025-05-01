b8af6567d9db244ee8b778210f2f57858e29ec0c
nico <nicolasmuniz2005@gmail.com>
2025-05-02 00:40:04 +0200

    Python path + Mejores nombres de secciones

--- init.lua
+++ init.lua
@@ -1,6 +1,9 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+-- Python manager
+vim.g.python3_host_prog = "/usr/bin/python3"
+
 vim.opt.backspace = '2'
 vim.opt.showcmd = true
 vim.opt.laststatus = 2
@@ -8,7 +11,7 @@ vim.opt.autowrite = true
 vim.opt.cursorline = true
 vim.opt.autoread = true
 
--- use spaces for tabs and whatnot
+-- Use spaces for tabs and whatnot
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.shiftround = true
@@ -19,7 +22,7 @@ vim.keymap.set("n", "<Leader>gg", ":copen | :silent :grep ")
 vim.keymap.set("n", "]q", ":cnext <CR>")
 vim.keymap.set("n", "[q", ":cprev <CR>")
 
--- Coloscheme and relative line numbers
+-- Colorscheme and relative line numbers
 --vim.cmd "colorscheme spacecamp_lite"
 vim.cmd "colorscheme xcodedarkhc"
 vim.cmd "set nu"
@@ -30,7 +33,7 @@ vim.cmd [[ set noswapfile ]]
 vim.cmd [[ set termguicolors ]]
 vim.cmd "set smartcase"
 
---Line numbers
+-- Line numbers
 vim.wo.number = true
 
 vim.g.loaded_netrw = 1
