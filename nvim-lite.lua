4004dc48049fb7cc97f861949117baadb2de4ced
nicomr <nicolasmuniz2005@gmail.com>
2025-08-29 02:27:19 +0200

    archivo de radleylewis completamente crudo

--- nvim-lite.lua
+++ nvim-lite.lua
@@ -4,7 +4,7 @@
 -- ================================================================================================
 
 -- theme & transparency
-vim.cmd[[colorscheme slate]]
+vim.cmd.colorscheme("unokai")
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
 vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
