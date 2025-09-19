97b5fb6d0a7c675da46509b918cdc396b12febdb
nicomr <nicolasmuniz2005@gmail.com>
2025-09-20 00:34:27 +0200

    Comentada linea que hace que los temas de colores interactúen raro con el tema propio de la terminal

--- lua/config/colorschemes.lua
+++ lua/config/colorschemes.lua
@@ -3,8 +3,8 @@
 -- ============================================================================
 
 -- Theme & transparency
-vim.cmd[[colorscheme slate]]
---vim.cmd[[colorscheme alduin]]
+--vim.cmd[[colorscheme slate]]
+vim.cmd[[colorscheme alduin]]
 --vim.cmd[[colorscheme slate]]
 --vim.cmd[[colorscheme gruvbox]]
 --vim.cmd[[colorscheme retrobox]]
@@ -14,7 +14,7 @@ vim.cmd[[colorscheme slate]]
 --vim.cmd[[colorscheme habamax]]
 --vim.cmd[[colorscheme spacecamp_lite]]
 --vim.cmd[[colorscheme xcodedarkhc]]
-vim.api.nvim_set_hl(0, "Normal", { bg = "none"})
+--vim.api.nvim_set_hl(0, "Normal", { bg = "none"})
 vim.api.nvim_set_hl(0, "NormalNC", { bg = "none"})
 vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none"})
 vim.o.background = "dark"
