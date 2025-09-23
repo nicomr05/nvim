2365b00bd0ad280f4c6d46051327ffe9d662e128
nicomr <nicolasmuniz2005@gmail.com>
2025-09-23 11:49:33 +0200

    Color del cursor inteligente

--- lua/config/colorschemes.lua
+++ lua/config/colorschemes.lua
@@ -17,4 +17,5 @@ vim.cmd[[colorscheme alduin]]
 --vim.api.nvim_set_hl(0, "Normal", { bg = "none"})
 vim.api.nvim_set_hl(0, "NormalNC", { bg = "none"})
 vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none"})
+vim.api.nvim_set_hl(0, "Cursor", { reverse })
 vim.o.background = "dark"
