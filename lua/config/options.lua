053a57e1d2cdc429cd3cc6d7a5002ea2cfc3a876
nicomr <nicolasmuniz2005@gmail.com>
2025-09-24 19:53:40 +0200

    La opción de winborder sigue petando en nvim v0.10.4 y anteriores

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -9,7 +9,7 @@ vim.opt.cursorline = true                          -- Highlight current line
 vim.opt.wrap = false                               -- Don't wrap lines
 vim.opt.scrolloff = 10                             -- Keep 10 lines above/below cursor 
 vim.opt.sidescrolloff = 8                          -- Keep 8 columns left/right of cursor
-vim.opt.winborder = 'rounded'
+--vim.opt.winborder = 'rounded'
 
 --vim.opt.showcmd = true
 --vim.opt.laststatus = 2
